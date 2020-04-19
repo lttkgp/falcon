accf5cb283390e4ae25c89c19ac047988f2fe93a
xypnox <xypnox@gmail.com>
2020-04-20 01:50:40 +0530

    Add scroll!
    And the favicon

---
+++ src/components/list.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import Card from './card';
+import { ChevronRight, ChevronLeft } from 'react-feather';
+
+const dummyData = [1, 2, 3, 4, 5, 6, 7, 8];
+
+type ListProps = {
+  title: string;
+};
+
+export default function List(props: ListProps) {
+  let [scrollLeft, setScrollLeft] = useState(false);
+  let [scrollRight, setScrollRight] = useState(true);
+
+  let hideArrows = (idname: string) => {
+    setTimeout(() => {
+      let el = document.getElementById(idname);
+      let scrollpp =
+        (100 * el?.scrollLeft!) / (el?.scrollWidth! - el?.clientWidth!);
+      console.log(scrollpp);
+      if (scrollpp > 99) {
+        setScrollRight(false);
+        setScrollLeft(true);
+      } else if (scrollpp < 1) {
+        setScrollLeft(false);
+        setScrollRight(true);
+      } else {
+        setScrollLeft(true);
+        setScrollRight(true);
+      }
+    }, 750);
+  };
+
+  return (
+    <div className='list'>
+      <h1 className='title'>{props.title}</h1>
+      <div className='array' id={props.title.trim()}>
+        {dummyData.map((num) => (
+          <Card id={num} key={num + 'IDHJH'} />
+        ))}
+      </div>
+      <div className={'scroll left' + (scrollLeft ? '' : ' hidden')}>
+        <ChevronLeft
+          onClick={() => {
+            let el = document.getElementById(props.title.trim());
+            el?.scrollBy({
+              top: 0,
+              left: -el.offsetWidth * 0.75,
+              behavior: 'smooth',
+            });
+            hideArrows(props.title);
+            setScrollLeft(true);
+            console.log(el?.scrollLeft, el?.offsetWidth);
+          }}
+        ></ChevronLeft>
+      </div>
+      <div className={'scroll right ' + (scrollRight ? '' : ' hidden')}>
+        <ChevronRight
+          onClick={() => {
+            let el = document.getElementById(props.title.trim());
+            el?.scrollBy({
+              top: 0,
+              left: el.offsetWidth * 0.75,
+              behavior: 'smooth',
+            });
+            setScrollRight(true);
+            console.log(el?.scrollLeft, el?.offsetWidth);
+            hideArrows(props.title);
+          }}
+        ></ChevronRight>
+      </div>
+    </div>
+  );
+}
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import List from '../components/list.js';
+import List from '../components/list';
 
 export default function Home(props: Object) {
   return (
     <div className='home'>
       <h1 className='header'>Home</h1>
-      <List />
-      <List />
-      <List />
+      <List title="KGP's picks 🔥" />
+      <List title='Top Weekly 🏅' />
+      <List title='Most Played Songs 🎵' />
+      <List title="Editor's Pick 👌" />
     </div>
   );
 }
