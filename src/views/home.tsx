1e181fff6fbc4243d96663a6a49a7ba22aa89ce4
xypnox <xypnox@gmail.com>
2020-06-02 18:01:00 +0530

    Add sample fetch data in Home

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import List from '../components/list.js';
 import Header from '../components/header';
 
 export default function Home(props: Object) {
+  useEffect(() => {
+    axios.get('https://api.lttkgp.com/v1/feed/latest?limit=20').then((data) => {
+      console.log(data);
+    });
+  });
+
   return (
     <div className='home'>
       <Header title='Home' />
