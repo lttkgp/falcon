18616713cef6e036849fa93a9278efac4083fa37
xypnox <xypnox@gmail.com>
2020-07-02 16:03:22 +0530

    Add basic structure for feed

---
+++ src/components/feed-card.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Heart } from 'react-feather';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { joinArtists } from '../utils';
+import { setQueue } from '../actions';
+
+type VideoProps = {
+  metadata: {
+    song: {
+      name: string;
+      image: string;
+    };
+    artists: [
+      {
+        name: string;
+        image: string;
+      }
+    ];
+    genre: [string];
+  };
+  link: string;
+  id: string;
+  post_count: number;
+  postdata: {
+    caption: string;
+    share_date: string;
+    permalink_url: string;
+    likes_count: number;
+  };
+};
+
+type CardProps = {
+  className: string;
+  onClick: Function;
+  redirect: boolean;
+  id: string;
+  gkey: string;
+  data: VideoProps;
+  queue: [VideoProps];
+  listName: string;
+};
+
+export default function FeedCard(props: CardProps) {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  return (
+    <div
+      className={props.className}
+      onClick={() => {
+        if (props.onClick !== undefined) {
+          props.onClick();
+        } else if (props.redirect === true) {
+          dispatch(setQueue(props.queue));
+          history.push('/video?=' + props.id);
+        }
+      }}
+    >
+      <img
+        src={'https://img.youtube.com/vi/' + props.id + '/mqdefault.jpg'}
+        alt=''
+      />
+      <div className='desc'>
+        <div className='text'>
+          {props.data.metadata ? (
+            <div className='di'>
+              <h1>{props.data.metadata.song.name}</h1>
+              <p>{joinArtists(props.data.metadata.artists)}</p>
+            </div>
+          ) : (
+            ''
+          )}
+        </div>
+        <div className='like'>
+          <Heart></Heart>
+          {props.data.postdata ? <p>{props.data.postdata.likes_count}</p> : ''}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+FeedCard.defaultProps = {
+  className: 'feed-card',
+};
--- src/views/feed.js
+++ src/views/feed.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { filterUniqueVideos } from '../utils';
+import FeedCard from '../components/feed-card';
+import styled from 'styled-components';
 
-export default function Feed(props: Object) {
+const FArray = styled.div`
+  grid-template-rows: repeat(${(props) => Math.ceil(props.len / 3)}, 1fr);
+`;
+
+export default function Feed() {
+  let [videoList, setVideoList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://api.lttkgp.com/v1/feed/latest?start=0&limit=50')
+      .then((resp) => {
+        if (resp.data) {
+          setVideoList(filterUniqueVideos(resp.data.posts));
+        }
+      });
+  }, []);
+
   return (
     <div className='feed'>
       <h1>Feed</h1>
+      <FArray className='feed-list' len={videoList.length}>
+        {videoList.map((vid) => (
+          <FeedCard
+            id={vid.id}
+            key={vid.id + 'feedX'}
+            data={vid}
+            queue={videoList}
+            redirect
+          />
+        ))}
+      </FArray>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam atque
         nisi, tempora debitis in dolorem porro laboriosam quaerat vero
