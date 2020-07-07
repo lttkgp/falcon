92c4999aa4d0ae050d038b59dae42b04b591694d
xypnox <xypnox@gmail.com>
2020-07-07 21:37:51 +0530

    Add genre page underdev notice

--- src/views/genre.tsx
+++ src/views/genre.tsx
@@ -1,56 +1,23 @@
 import React from 'react';
+import underDev from '../images/under-dev.png';
 
 export default function Genre(props: Object) {
   return (
-    <div className='genre'>
+    <div className='genre-page'>
       <h1>Genre</h1>
+      <img src={underDev} alt='' />
+      <h2>Under Development</h2>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam atque
-        nisi, tempora debitis in dolorem porro laboriosam quaerat vero
-        blanditiis rem voluptates omnis odit et architecto sint aliquid quisquam
-        at.
+        Head to{' '}
+        <a href='https://github.com/lttkgp/'>
+          our Github Organisation to contribute!
+        </a>
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sit
-        quaerat amet facilis fugit ipsam corrupti reiciendis vitae laboriosam ab
-        dolore quis nihil ut modi, corporis voluptatum aspernatur consectetur
-        officiis?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam atque
-        nisi, tempora debitis in dolorem porro laboriosam quaerat vero
-        blanditiis rem voluptates omnis odit et architecto sint aliquid quisquam
-        at.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sit
-        quaerat amet facilis fugit ipsam corrupti reiciendis vitae laboriosam ab
-        dolore quis nihil ut modi, corporis voluptatum aspernatur consectetur
-        officiis?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam atque
-        nisi, tempora debitis in dolorem porro laboriosam quaerat vero
-        blanditiis rem voluptates omnis odit et architecto sint aliquid quisquam
-        at.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sit
-        quaerat amet facilis fugit ipsam corrupti reiciendis vitae laboriosam ab
-        dolore quis nihil ut modi, corporis voluptatum aspernatur consectetur
-        officiis?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam atque
-        nisi, tempora debitis in dolorem porro laboriosam quaerat vero
-        blanditiis rem voluptates omnis odit et architecto sint aliquid quisquam
-        at.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sit
-        quaerat amet facilis fugit ipsam corrupti reiciendis vitae laboriosam ab
-        dolore quis nihil ut modi, corporis voluptatum aspernatur consectetur
-        officiis?
+        We discuss the development at{' '}
+        <a href='https://join.slack.com/t/listentothiskgp/shared_invite/zt-ej4jij2r-zZrqAn6yE2hxL6J1iudlNA'>
+          our Slack workspace
+        </a>
       </p>
     </div>
   );
