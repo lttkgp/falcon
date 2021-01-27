6d3f0ef9371d48a411d3f9afde9f4493814d5703
Sahil Shubham <sahil.shubham2000@gmail.com>
2021-01-27 18:12:30 +0530

    added markdown; rendered markdown;

---
+++ public/aboutPage.md
@@ -0,0 +1,10 @@
+Listen To This KGP (LTTKGP) is a music streaming platform that brings together people using the songs that recommend on our Facebook page. This is was a join effort by few students on IIT KGP, but the developer base is consistently getting larger due to the this being an Open Source platform.
+
+## Features of LTTKGP
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, magnam. Minima, molestiae amet similique mollitia porro expedita officia delectus eligendi magnam placeat, earum accusantium veritatis nobis voluptate tempore omnis quas unde saepe excepturi. Vel autem dolore expedita! Porro tempore, inventore veniam distinctio laboriosam animi a odit sint repellat, voluptatem pariatur.
+
+## Let's Improve this together 
+
+Let us know what you think by talking to us directly through our [Slack workspace](https://join.slack.com/t/listentothiskgp/shared_invite/zt-l4pt2o1z-~qDftV6hhSmY1V5Y1GsL3g). You can also see how all of this comes together and works at the programming level by taking a look at our [Github repositories](https://github.com/lttkgp)
+
--- src/views/about.tsx
+++ src/views/about.tsx
@@ -1,34 +1,25 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 import ReactMarkdown from "react-markdown";
 import Header from "../components/header";
 import gfm from "remark-gfm";
 
-const changelogs = `
-# Changelogs
-- this is for testing purposes
-- try to delocalize this markdown
-`;
-
 export const About = () => {
+    const [text, setText] = useState('');
+    
+    useEffect(() => {
+        async function getText() {
+        const markdown = await fetch("/aboutPage.md");
+            const text = await markdown.text();
+            
+            setText(text);
+        }
+        getText();
+    }, []);
+
     return (
         <div className="about-page">
             <Header title="About LTTKGP" />
-            <p>
-                Listen To This KGP (LTTKGP) is a music streaming platform that brings together people using the songs that recommend on our
-                Facebook page. This is was a join effort by few students on IIT KGP, but the developer base is consistently getting larger due 
-                to the this being an Open Source platform.
-            </p>
-
-            <h2>Features of LTTKGP</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, magnam. Minima, molestiae amet similique mollitia porro expedita officia delectus eligendi magnam placeat, earum accusantium veritatis nobis voluptate tempore omnis quas unde saepe excepturi. Vel autem dolore expedita! Porro tempore, inventore veniam distinctio laboriosam animi a odit sint repellat, voluptatem pariatur.</p>
-
-            <h2>Let's improve this together</h2>
-            <p>
-                Let us know what you think by talking to us directly through our <a href="https://join.slack.com/t/listentothiskgp/shared_invite/zt-l4pt2o1z-~qDftV6hhSmY1V5Y1GsL3g" target="_blank" rel="noreferrer noopener">Slack workspace</a>. 
-                You can also see how all of this comes together and works at the programming level by taking a look at our <a href="https://github.com/lttkgp" target="_blank" rel="noreferrer noopener">Github repositories</a>.
-            </p>
-            
-            <ReactMarkdown plugins={[gfm]} children={changelogs} />
+            <ReactMarkdown plugins={[gfm]} children={text} />
         </div>
     );
 }
