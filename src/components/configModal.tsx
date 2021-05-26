4a41ef1b3de8d2e99952235984320400ec5e4481
xypnox <xypnox@gmail.com>
2021-05-27 02:12:04 +0530

    ̦Add cssTransitions and theme for modal

--- src/components/configModal.tsx
+++ src/components/configModal.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import * as Icon from "react-feather";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import styled from "styled-components";
 import { changeTheme } from "../utils";
+
+const Modal = styled.div``;
 
 export default function ConfigModal() {
   const [modalOpen, setModalOpen] = useState(false);
@@ -8,43 +12,51 @@ export default function ConfigModal() {
   const toggle = (e: any) => {
     setModalOpen(() => !modalOpen);
   };
+
+  const hoverClose = (e: any) => {
+    if (modalOpen) setModalOpen(() => !modalOpen);
+  };
+
   return (
-    <div className="configModal-wrapper">
+    <Modal className="configModal-wrapper">
       <div className="sidebar-button" onClick={toggle}>
         <Icon.Menu className="sidebar-icon"></Icon.Menu>
       </div>
-      <div className={`configModal ${modalOpen ? "open" : "close"}`}>
-        <a href="" target="_blank" rel="noopener noreferrer" className="icon-link">
-          <Icon.Info></Icon.Info>
-          About LTTKGP
-        </a>
-        <a href="" target="_blank" rel="noopener noreferrer" className="icon-link">
-          <Icon.GitHub></Icon.GitHub>
-          Code @ Github
-        </a>
-        <a href="" target="_blank" rel="noopener noreferrer" className="icon-link">
-          <Icon.Slack></Icon.Slack>
-          Join us on Slack
-        </a>
-        <a href="" target="_blank" rel="noopener noreferrer" className="icon-link">
-          <Icon.Facebook></Icon.Facebook>
-          Facebook Group
-        </a>
-        <hr />
-        <a href="" target="_blank" rel="noopener noreferrer" className="icon-link">
-          <Icon.CheckCircle />
-          Posts are up to date.
-        </a>
-        <div className="theme-switcher" onClick={changeTheme}>
-          <div className="sidebar-button">
-            <div className="sidebar-icon change-theme-icon"></div>
+
+      <CSSTransition key={"ok"} in={modalOpen} className="configModal" classNames="configModal" timeout={300}>
+        <div onMouseLeave={hoverClose}>
+          <a href="" target="_blank" rel="noopener noreferrer" className="icon-link">
+            <Icon.Info></Icon.Info>
+            <div className="link_text">About LTTKGP</div>
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer" className="icon-link">
+            <Icon.GitHub></Icon.GitHub>
+            <div className="link_text">Code @ Github</div>
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer" className="icon-link">
+            <Icon.Slack></Icon.Slack>
+            <div className="link_text">Join us on Slack</div>
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer" className="icon-link">
+            <Icon.Facebook></Icon.Facebook>
+            <div className="link_text">Facebook Group</div>
+          </a>
+          <hr />
+          <a href="" target="_blank" rel="noopener noreferrer" className="icon-link">
+            <Icon.CheckCircle />
+            <div className="link_text">Posts are up to date.</div>
+          </a>
+          <div className="theme-switcher" onClick={changeTheme}>
+            <div className="sidebar-button">
+              <div className="sidebar-icon change-theme-icon"></div>
+            </div>
+            <div>SWITCH THEME</div>
           </div>
-          <div>SWITCH THEME</div>
+          <div className="sidebar-button close-icon">
+            <Icon.X className="sidebar-icon" onClick={toggle}></Icon.X>
+          </div>
         </div>
-        <div className="sidebar-button close-icon">
-          <Icon.X className="sidebar-icon" onClick={toggle}></Icon.X>
-        </div>
-      </div>
-    </div>
+      </CSSTransition>
+    </Modal>
   );
 }
