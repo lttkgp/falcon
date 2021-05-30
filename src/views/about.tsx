118d0a4f70f35982bba6e0f7e762579385e5b98c
Naresh <ghostwriternr@gmail.com>
2021-05-30 21:55:24 +0530

    Fix link to about page

--- src/views/about.tsx
+++ src/views/about.tsx
@@ -1,25 +1,25 @@
-import React, { useState, useEffect }from "react";
-import ReactMarkdown from "react-markdown/with-html";
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
 import Header from "../components/header";
 import gfm from "remark-gfm";
 
 export const About = () => {
-    const [text, setText] = useState('');
-    
-    useEffect(() => {
-        async function getText() {
-        const markdown = await fetch("/aboutPage.md");
-            const text = await markdown.text();
-            
-            setText(text);
-        }
-        getText();
-    }, []);
+  const [text, setText] = useState("");
 
-    return (
-        <div className="about-page">
-            <Header title="About LTTKGP" />
-            <ReactMarkdown plugins={[gfm]} children={text} allowDangerousHtml />
-        </div>
-    );
-}
+  useEffect(() => {
+    async function getText() {
+      const markdown = await fetch("/aboutPage.md");
+      const text = await markdown.text();
+
+      setText(text);
+    }
+    getText();
+  }, []);
+
+  return (
+    <div className="about-page">
+      <Header title="About LTTKGP" />
+      <ReactMarkdown plugins={[gfm]} children={text} />
+    </div>
+  );
+};
